52c0e67d718e4e3f8926032d2c740ca374fd0c30
kumarvishal22665 <johnealexe111@gmail.com>
2025-07-16 19:54:24 +0530

    today we are add two files in my repo

---
+++ script.js
@@ -0,0 +1,21 @@
+function printMessage(message) {
+    console.log(message);
+}
+
+printMessage("Hello, World!");
+printMessage("This is a simple JavaScript function.");
+printMessage("Let's print another message.");
+printMessage("Goodbye!");
+printMessage("This is the end of the script.");
+printMessage("Thank you for using this script!");
+printMessage("Feel free to modify the messages.");
+printMessage("Have a great day!");
+printMessage("Remember to stay positive!");
+printMessage("Keep learning and coding!");
+printMessage("JavaScript is fun!");
+printMessage("Happy coding!");
+printMessage("This is a test message.");
+printMessage("You can add more messages here.");
+printMessage("This is a sample message for demonstration.");
+printMessage("Enjoy your coding journey!");
+printMessage("Let's keep the momentum going!");
